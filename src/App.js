25ce4f32653062de47251e5f34e4798538210f17
Nisha <nizfaruqui512@gmail.com>
2019-04-17 19:30:45 -0400

    Working Counter program

--- src/App.js
+++ src/App.js
@@ -3,19 +3,36 @@ import React, {Component} from 'react';
 class Counter extends Component {
   constructor() {
     super();
-    this.state = {
+    this.state = { //state is data that changes
       count: 0,
     };
   }
+
+/* here we write custom functionality
+Increment is a function that will allow us to increment the Counter
+Decrement is a function that will allow us to decrement the counter
+ */
+
+ increment = () => {
+   this.setState ({ /* setState is a built in method for React */
+     count: this.state.count + 1
+   })
+ }
+
+decrement = () => {
+  this.setState ({
+    count: this.state.count - 1
+  })
+}
 
   render() {
     return(
       <div className="container">
         <div className="navbar">Counter.js</div>
         <div className="counter">
-          <h1>What do we put here?</h1>
-          <button type="button">Increment</button>
-          <button type="button">Decrement</button>
+          <h1>{this.state.count}</h1>
+          <button type="button" onClick={this.increment}>Increment</button>
+          <button type="button"onClick={this.decrement}>Decrement</button>
         </div>
       </div>
     ); //semicolon needed?
